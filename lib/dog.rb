1fa1f9d2a9ea9fad0f7595212d3925e45d9d636f
wendy <mizukilee@gmail.com>
2023-01-10 20:34:30 -0800

    commit tree 17894a909eb8786574d3ca129cfaef8c0e972e3f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    attr_accessor :name, :breed
+    # Getter 
+    # def name 
+    #     @name 
+    # end
 
+    # # def breed 
+    # #     @breed 
+    # # end
+
+    # Setter 
+    # def name = name
+    #     @name = name
+    # end
+
+    # def breed = (breed)
+    #     @breed = breed
+    # end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,4 @@
 class Person
-
+    attr_reader :name, :job 
+    attr_writer :name, :job 
 end
